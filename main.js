e176f13fe0c3970087a56f63ef1b0b7747eb82b7
animaaryan <aaryan.junnarkar@gmail.com>
2026-01-02 14:18:11 +0530

    Adding stylesheet

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // glTF loader
@@ -7,34 +7,53 @@ const loader = new GLTFLoader();
 
 var model;
 
-loader.load( 'public/models/web_logo_test.glb', function ( gltf ) {
+loader.load('public/models/web_logo_test.glb', function (gltf) {
   model = gltf.scene;
-  scene.add( model );
+  scene.add(model);
 
-}, undefined, function ( error ) { 
+}, undefined, function (error) {
 
-  console.error( error );
+  console.error(error);
 
-} );
+});
+
+// Set variables for the sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
 // Setup a scene for the webpage
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const canvas = document.getElementById("experience-canvas");
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(sizes.width, sizes.height);
 document.body.appendChild(renderer.domElement);
 
-camera.position.z = 5;
+// Change the camera position on the Z axis
+camera.position.z = 1;
 
+// Handle window resize
+function handleResize() {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+}
+
+window.addEventListener("resize", handleResize);
 
 // Render the scene
 function animate() {
-    if (model) {
-        model.rotation.x += 0.01; 
-        model.rotation.y += 0.01;
-    }
-    renderer.render(scene, camera);
+  if (model) {
+    model.rotation.x += 0.001;
+    model.rotation.y += 0.001;
+  }
+  renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
 
